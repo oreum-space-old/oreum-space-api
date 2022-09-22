703bad12e4385e4c9535f1662b0e5c9d718bcf43
oredan <179danil@gmail.com>
2022-09-22 15:17:26 +0300

    developing

--- src/utils/processLog.ts
+++ src/utils/processLog.ts
@@ -34,7 +34,7 @@ function createLog (level: Level) {
         process.module || 'null'
       } | ${
         process.action || 'null'
-      } ] ${outputs.join('\n')}\n`
+      } ] ${outputs.join('\n  ')}\n`
     )
   }
 }
