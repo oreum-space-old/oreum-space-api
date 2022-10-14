8727296a8440ba48276bb92fcab8a56329190052
oredan <179danil@gmail.com>
2022-10-14 11:05:45 +0300

    update 14.10.22 fix discord integration endpoint 5

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { json } from 'express'
 import api from './api'
 import application from './application'
 import discord from './discord'
+import verifyDiscordRequest from './discord/utils/verifyDiscordRequest'
 import mongoose from './mongoose'
 import app from './utils/app'
 import appMounted from './utils/app-mounted'
@@ -15,6 +16,7 @@ app
   .use(json())          // JSON
   .use(cookieParser())  // Cookie
   // .use(useCors)         // Cors
+  .use(verifyDiscordRequest('/discord/integrations'))
   .get(...ping())       // /path
   .reg(api)             // /api
   .reg(discord)         // /discord
