07fdb27460fe09b17339d9c5c0f31877330e1b93
oredan <179danil@gmail.com>
2022-10-14 12:16:24 +0300

    update 14.10.22 cors returned

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ import app from './utils/app'
 import appMounted from './utils/app-mounted'
 import errorMiddleware from './utils/middlewares/error-middleware'
 import ping from './utils/ping'
-// import useCors from './utils/useCors'
+import useCors from './utils/useCors'
 
 app
   .use(express.json({ verify: createVerifyDiscordRequest(process.env.DISCORD_PUBLIC_KEY!, '/discord/interactions') }))
   .use(json())          // JSON
   .use(cookieParser())  // Cookie
-  // .use(useCors)         // Cors
+  .use(useCors)         // Cors
   .get(...ping())       // /path
   .reg(api)             // /api
   .reg(discord)         // /discord
