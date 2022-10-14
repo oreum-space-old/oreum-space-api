4c109194a45ed9573be6a9b9a1c7e374a36ccbf9
oredan <179danil@gmail.com>
2022-10-14 11:22:04 +0300

    update 14.10.22 fix discord integration endpoint 6

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,11 +1,10 @@
-import express, { Router } from 'express'
+import { Router } from 'express'
 import { ModuleOptions } from '../utils/app'
 import pingUtil from '../utils/ping'
 import updateGuildCommands, { InitCommand } from './commands'
 import dice from './commands/dice'
 import test from './commands/test'
 import interactions from './interactions'
-import { createVerifyDiscordRequest } from './utils/verifyDiscordRequest'
 
 const discordEnabled = process.env.DISCORD_ENABLED?.toLowerCase() === 'true'
 
@@ -41,9 +40,8 @@ const discord: ModuleOptions = {
   condition: discordEnabled,
   endpoint: discordEndpoint,
   router,
-  beforeCreate (app) {
+  beforeCreate () {
     process.setModule(discord.module)
-    app.use(express.json({ verify: createVerifyDiscordRequest(process.env.DISCORD_PUBLIC_KEY!, '/discord/integrations') }))
     return discord
   },
   async mount () {
