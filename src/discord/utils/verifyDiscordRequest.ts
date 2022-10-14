75cacf12d0ea2bde5e270f6a9d78534864634e3b
oredan <179danil@gmail.com>
2022-10-14 11:51:41 +0300

    update 14.10.22 fix discord integration endpoint 11

--- src/discord/utils/verifyDiscordRequest.ts
+++ src/discord/utils/verifyDiscordRequest.ts
@@ -3,20 +3,21 @@ import { Request, Response } from 'express'
 
 export function createVerifyDiscordRequest (clientKey: string, endpoint: string) {
   return function (request: Request, response: Response, buffer: Buffer): void {
-    process.setModule('discord').info('verifyDiscordRequest', endpoint, request.url)
+    process.setModule('discord').info(`from verifyDiscordRequest: { request.url: ${request.url}, verifiableEndpoints: ${endpoint} }`)
     if (request.url.includes(endpoint)) {
-      process.setModule('discord').log(`[${request.method}] ${request.url} - Verifying Discord Request`)
+      process.log(`[${request.method}] ${request.url} - Verifying Discord Request`)
 
       const signature = request.get('X-Signature-Ed25519')
       const timestamp = request.get('X-Signature-Timestamp')
-      process.log('\nsignature:', signature, '\ntimestamp:', timestamp)
+      process.info(`verifying Discord Request with data: \n  ${signature}\n  ${timestamp}`)
 
       const isValidRequest = verifyKey(buffer, signature!, timestamp!, clientKey)
       if (!isValidRequest) {
+        process.error('Discord request invalid!')
         response.status(401).send('Bad request signature')
         throw new Error('Bad request signature')
       }
-      process.log('Discord request verified!')
+      process.info('Discord request verified!')
     } else {
       process.setModule('discord').log(`[${request.method}] ${request.url} - not discord url`)
     }
